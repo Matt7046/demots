9507ca8efa4edb13ab0ca971a0c10f5cc015558b
Matt7046 <matteo.santangelo@colorsdev.tech>
2024-12-13 14:43:21 +0100

    componenti pulsanti affiancati

--- src/components/pageSubPromise/SubPromiseContent.tsx
+++ src/components/pageSubPromise/SubPromiseContent.tsx
@@ -68,7 +68,7 @@ const SubPromiseContent: React.FC<any> = ({
   return (
     <>
       <div className={`row`}>
-        <div className="col-display" style={{ gridColumn: 'span 10' }}>
+        <div className="col-display" style={{ gridColumn: 'span 9' }}>
           <NomeDisplay
             rowIndex={rowIndex}
             handleClick={() => clickRowNome(rowIndex)}
@@ -79,10 +79,10 @@ const SubPromiseContent: React.FC<any> = ({
         <div
           className="col-button-container"
           style={{
-            gridColumn: 'span 2', // Unisce le colonne 11 e 12
+            gridColumn: 'span 3', // Unisce le colonne 11 e 12
             display: 'grid', // Utilizza il grid per disporre i pulsanti
-            //    gridTemplateColumns: '1fr 1fr', // Due colonne uguali
-            gap: '10px', // Distanza tra i pulsanti
+            gridTemplateColumns: '2fr 1fr', // Due colonne uguali
+            gap: '1px', // Distanza tra i pulsanti
             visibility: visibiityButton ? 'visible' : 'hidden',
           }}
          >
